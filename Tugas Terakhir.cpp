a35f7b3ed214cb5f7f14ccb7b1ad8a6e7e879826
AhmadSyaifuddin <32382801+AhmadSyaifuddin@users.noreply.github.com>
2018-01-20 22:10:46 +0700

    membuat objek class motor

--- Tugas Terakhir.cpp
+++ Tugas Terakhir.cpp
@@ -23,5 +23,43 @@ class motor{
  }
  
  int main(){
- 	
+ 	//membuat objek class
+ 	motor motorBalap, motorDrag, motorSuperbike;
+ 	//memberi nilai
+ 	//drag
+ 	motorDrag.JumlahMotor= 150;
+ 	motorDrag.merek = "kawasaki";
+ 	//balap
+ 	motorBalap.JumlahMotor = 100;
+ 	motorBalap.merek = "yamaha";
+ 	//bebek
+ 	motorSuperbike.JumlahMotor = 200;
+ 	motorSuperbike.merek = "ducati";
+ 	string pilihan;
+ 	//
+ 	cout << "pilih motor : "; cin >> pilihan;
+ 	//
+ 	if (pilihan== "motorBalap"){
+ 		cout << "Motor Balap : "<<endl;
+ 		cout << "jumlah motor = "<<(motorBalap.JumlahMotor)<<"unit "<<endl;
+ 		cout << "Merek Motor =  "<<(motorBalap.merek)<<endl;
+ 	motorBalap.racing("250km/jam");
+	motorBalap.gantiKampasrem("kampas new");
+	}else if(pilihan=="motorDrag"){
+		cout << "Motor drag : "<<endl;
+		cout << "jumlah motor = "<<(motorDrag.JumlahMotor)<<" unit "<<endl;
+		cout << "Merek Motor =  "<<(motorDrag.merek)<<endl;
+	motorDrag.racing("300km/jam");
+	motorDrag.gantiKampasrem("generalCT");
+		
+	}else if(pilihan=="motorSuperbike"){
+		cout << "Motor Superbike : "<<endl;
+		cout << "jumlah motor = "<<(motorSuperbike.JumlahMotor)<<"unit"<<endl;
+		cout << "Merek Motor =  "<<(motorSuperbike.merek)<<endl;
+	motorSuperbike.racing("500km/jam");
+	motorSuperbike.gantiKampasrem("brembo");
+    }else{
+         cout<< "error";
+  }
+	
  }
